9470cc6b9c1c9fc8b0c36ab8a2ee49632f473fbb
Cybernetics <mail@cybernetics.me>
2023-12-11 23:24:19 +0530

    small updates

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -14,6 +14,27 @@ class PdfController extends Controller
         $consultation = Consultation::findOrFail(decrypt($id));
         $qrcode = base64_encode(QrCode::format('svg')->size(50)->errorCorrection('H')->generate(qrCodeText()));
         $pdf = PDF::loadView('backend.pdf.prescription.all', compact('consultation'));
+        $pdf->output();
+        $canvas = $pdf->getDomPDF()->getCanvas();
+
+        $height = $canvas->get_height();
+        $width = $canvas->get_width();
+
+        $canvas->set_opacity(.2, "Multiply");
+
+        $canvas->set_opacity(.2);
+
+        $canvas->page_text(
+            $width / 5,
+            $height / 2,
+            'Medical Prescription Pro',
+            null,
+            55,
+            array(0, 0, 0),
+            2,
+            2,
+            -30
+        );
         return $pdf->stream($consultation->medical_record_number . '.pdf');
     }
 }
